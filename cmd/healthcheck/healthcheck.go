a348f579930f25654e664a05131af615ad83c4e1
Jesús Diéguez Fernández <jesusdf@gmail.com>
2023-03-12 01:54:38 +0100

    Make the status code configurable in the healthcheck script.

--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -6,19 +6,30 @@ import (
   "os"
   "crypto/tls"
   "net/http"
+  "strconv"
 )
 
 func main() {
+
    http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-   resp, err := http.Get(os.Args[1])
+
+   expectedCode, err := strconv.Atoi(os.Args[1])
+
    if err != nil {
     panic(err)
     os.Exit(-1)
    }
 
-   if resp.StatusCode == 302 {
+   resp, err := http.Get(os.Args[2])
+
+   if err != nil {
+    panic(err)
+    os.Exit(-2)
+   }
+
+   if resp.StatusCode == expectedCode {
 	  os.Exit(0)
    } else {
-	  os.Exit(-2)
+	  os.Exit(-3)
    }
 }
