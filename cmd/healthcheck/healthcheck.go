dbbda1ca07b0187a4e9da4a31759b1c540cf9767
Jesús Diéguez Fernández <jesusdf@gmail.com>
2023-03-13 10:27:55 +0100

    Show more information when healthcheck fails.

--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -3,38 +3,36 @@
 package main
 
 import (
-  "os"
-  "crypto/tls"
-  "net/http"
-  "strconv"
-  "fmt"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-   http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-   expectedCode, err := strconv.Atoi(os.Args[1])
+	expectedCode, err := strconv.Atoi(os.Args[1])
 
-   if err != nil {
-	panic(err)
-	os.Exit(-1)
-   }
+	if err != nil {
+		panic(err)
+	}
 
-   resp, err := http.Get(os.Args[2])
+	url := os.Args[2]
+	resp, err := http.Get(url)
 
-   if err != nil {
-	panic(err)
-	os.Exit(-2)
-   }
+	if err != nil {
+		panic(err)
+	}
 
-   if resp.StatusCode == expectedCode {
-	fmt.Printf("[OK]\n")
-	os.Exit(0)
-   } else {
-	fmt.Printf("[KO] Got status code %v and it should be %v.\n", resp.StatusCode, expectedCode)
-	os.Exit(-3)
-   }
+	if resp.StatusCode == expectedCode {
+		fmt.Printf("[OK]\n")
+		os.Exit(0)
+	} else {
+		fmt.Printf("[KO] Got status code %v when accessing %v, and it should be %v.\n\n%v", resp.StatusCode, url, expectedCode, resp)
+		os.Exit(-1)
+	}
 
 }
-
